74cac060a22ac4b1812577dc3bb410f0b878d4bd
Aptcoder <omilosamuel@gmail.com>
2021-02-26 09:56:38 +0100

    chore: initialized git repo

---
+++ FlaskExercise/__init__.py
@@ -0,0 +1,16 @@
+import logging
+from flask import Flask
+
+
+
+app = Flask(__name__)
+wsgi_app = app.wsgi_app
+# TODO: Set the app's logger level to "warning"
+#   and any other necessary changes
+
+app.logger.setLevel(logging.WARNING)
+streamHandler = logging.StreamHandler()
+streamHandler.setLevel(logging.WARNING)
+app.logger.addHandler(streamHandler)
+
+import FlaskExercise.views
---
+++ FlaskExercise/views.py
@@ -0,0 +1,21 @@
+from flask import flash, render_template, redirect, request
+from FlaskExercise import app
+
+
+@app.route('/')
+def home():
+    log = request.values.get('log_button')
+    # TODO: Appropriately log the different button presses
+    #   with the appropriate log level.
+    if log == 'warning':
+        app.logger.warning('%s', log)
+    elif log == 'info':
+        app.logger.info('%s', log)
+    elif log == 'error':
+        app.logger.error('%s', log)
+    else:
+        app.logger.critical('%s', log)
+    return render_template(
+        'index.html',
+        log=log
+    )
